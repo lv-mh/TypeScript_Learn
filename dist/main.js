76eaf5276cf1c774addba0b960e124c618a7fbb3
lvmh <lvminghui1001@126.com>
2020-10-21 22:10:45 +0800

    修改rconfig

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var greet_1 = require("./greet");
 function showHello(divName, name) {
     var elt = document.getElementById(divName);
-    elt.innerText = greet_1.sayHello(name);
+    var decLiteral = 6;
+    elt.innerText = greet_1.sayHello(name + " " + decLiteral + "aha");
 }
 showHello("greeting", "TypeScript");
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { sayHello } from "./greet";
 
 function showHello(divName: string, name: string) {
-    const elt = document.getElementById(divName);
+    let elt = document.getElementById(divName);
     let decLiteral: number = 6;
-    elt.innerText = sayHello(name+" "+decLiteral);
+    elt.innerText = sayHello(name+" "+decLiteral+"aha");
 
 }
 
