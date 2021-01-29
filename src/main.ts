84e89347f42442893203d26f7d0f05549fd55509
lvmh <lvminghui1001@126.com>
2021-01-29 17:44:35 +0800

    修改

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { sayHello } from "./greet";
 
 function showHello(divName: string, name: string) {
-    let elt = document.getElementById(divName);
-    let decLiteral: number = 6;
-    elt.innerText = sayHello(name+" "+decLiteral+"aha");
-
+    const elt = document.getElementById(divName);
+    elt.innerText = sayHello(name);
 }
 
 showHello("greeting", "TypeScript");
